8a0cf26dd38281d26d9a78ff4f312bc190ceda81
gabbe-k <44163424+gabbe-k@users.noreply.github.com>
2019-03-04 21:47:44 +0100

    need to fix big styling issue involving bottom margins, checkmark animation fixed

--- view/listview.php
+++ view/listview.php
@@ -1,15 +1,40 @@
 <div class="listview-wrap">
 
-  <div class="request">
-    <div class="request-checkmark">
-      <p>H</p>
+  <div class="request-wrap">
+
+    <div class="request">
+      <div class="request-checkmark">
+      </div>
+      <div class="request-username-wrap">
+        <p>Username</p>
+      </div>
+      <div class="request-text-wrap">
+        <p>This is a big text</p>
+      </div>
     </div>
-    <div class="request-username-wrap">
-      <p>Username</p>
+
+    <div class="request">
+      <div class="request-checkmark">
+      </div>
+      <div class="request-username-wrap">
+        <p>Username</p>
+      </div>
+      <div class="request-text-wrap">
+        <p>This is a big text</p>
+      </div>
     </div>
-    <div class="request-text-wrap">
-      <p>This is a big text</p>
+
+    <div class="request">
+      <div class="request-checkmark">
+      </div>
+      <div class="request-username-wrap">
+        <p>Username</p>
+      </div>
+      <div class="request-text-wrap">
+        <p>This is a big text</p>
+      </div>
     </div>
+
   </div>
 
 </div>

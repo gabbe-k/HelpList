9ca134a5bd8377fe9fac51f3b43c6a4351af751b
gabbe-k <44163424+gabbe-k@users.noreply.github.com>
2019-03-04 12:20:04 +0100

    Push 2

--- view/listview.php
+++ view/listview.php
@@ -1,3 +1,15 @@
 <div class="listview-wrap">
-  
+
+  <div class="request">
+    <div class="request-checkmark">
+      <p>H</p>
+    </div>
+    <div class="request-username-wrap">
+      <p>Username</p>
+    </div>
+    <div class="request-text-wrap">
+      <p>This is a big text</p>
+    </div>
+  </div>
+
 </div>

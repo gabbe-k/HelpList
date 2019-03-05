78bb1544086404657921310a2940373422e10f4e
gabbe-k <gabbekall@gmail.com>
2019-03-05 16:27:32 +0100

    Js checkmark logic begun, array not working though

--- js/listview.js
+++ js/listview.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 
   $('.listview-wrap').click(function(event) {
 
+      var checkedElementArr = [];
+
       if ($(event.target).is('.request-checkmark')) {
 
         var checkmark = event.target;
@@ -11,15 +13,26 @@ $(document).ready(function() {
 
           $(checkmark).removeClass("animatable--now");
           $(checkmark).removeClass("isChecked");
+          checkedElementArr = jQuery.grep(checkedElementArr, function(value) {
+            return value != checkmark;
+          });
 
         }
         else {
 
+          checkedElementArr.push(checkmark);
           $(checkmark).addClass("animatable--now");
           $(checkmark).addClass("isChecked");
 
         }
 
+      }
+
+      if ($(event.target).is('#request-form')) {
+
+        console.log(checkedElementArr[0]);
+        console.log(checkedElementArr[1]);
+        console.log(checkedElementArr[2]);
       }
 
     });

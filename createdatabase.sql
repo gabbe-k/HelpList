03d1f7840d680b04c6a27d3d9562f790fdfeef38
gabbe-k <44163424+gabbe-k@users.noreply.github.com>
2019-03-05 13:15:43 +0100

    sql script completed, print function added

--- createdatabase.sql
+++ createdatabase.sql
@@ -1,5 +1,8 @@
 DROP TABLE Users;
 DROP TABLE Requests;
+DROP DATABASE helplist;
+
+CREATE DATABASE helplist;
 
 CREATE TABLE Users(
 	id int(10),
--- view/listview.php
+++ view/listview.php
@@ -10,38 +10,7 @@
 
   <div class="request-wrap">
 
-    <div class="request">
-      <div class="request-checkmark">
-      </div>
-      <div class="request-username-wrap">
-        <p>Username</p>
-      </div>
-      <div class="request-text-wrap">
-        <p>This is a big text</p>
-      </div>
-    </div>
-
-    <div class="request">
-      <div class="request-checkmark">
-      </div>
-      <div class="request-username-wrap">
-        <p>Username</p>
-      </div>
-      <div class="request-text-wrap">
-        <p>This is a big text</p>
-      </div>
-    </div>
-
-    <div class="request">
-      <div class="request-checkmark">
-      </div>
-      <div class="request-username-wrap">
-        <p>Username</p>
-      </div>
-      <div class="request-text-wrap">
-        <p>This is a big text</p>
-      </div>
-    </div>
+    <?php require_once('././php/print/prreq.php') ?>
 
     <div class="request-formsection-outer-wrap">
         <div class="request-form-wrap">

cea0109d41d8e603390d2f17e663ad7ee8b8736e
OlliverLight <48206915+OlliverLight@users.noreply.github.com>
2019-03-06 10:12:53 +0100

    Update createdatabase.sql

--- createdatabase.sql
+++ createdatabase.sql
@@ -9,14 +9,13 @@ CREATE TABLE Users(
     email varchar(10),
     uid text,
     pwd text,
-    isTeachr int
+    isTeachr int --< bit? >--
 );
 
 INSERT INTO `users` (`id`, `email`, `uid`, `pwd`, `isTeachr`) VALUES ('100', 'bigman', 'bigman', 'bigman', '0');
 
 CREATE TABLE Requests(
 	id int(10),
-		postId int(10),
     reqText text,
     idTeachr int(10)
 );

0f854fd839e4f0e701c183887fa0d51045e90aad
gabbe-k <gabbekall@gmail.com>
2019-03-06 14:06:15 +0100

    kind of better scaling? Signup form stylized

--- view/signup.php
+++ view/signup.php
@@ -10,17 +10,18 @@
 
   <div class="request-wrap">
 
-      <div class="#">
-        <section class="#">
-          <h1>Signup</h1>
+      <div class="listview-signup-wrap">
+
+        <div class="listview-signup-form-wrap">
           <form action="Inc/signup.inc.php" method="POST">
             <input type="text" name="uid" placeholder="Username">
             <input type="text" name="mail" placeholder="E-mail">
             <input type="password" name="pwd" placeholder="Password">
             <input type="password" name="pwd-repeat" placeholder="Repeat password">
-            <button type="submit" name="signup-submit">Signup</button>
+            <input type="submit" name="signup-submit" value="Submit">
           </form>
-        </section>
+        </div>
+
       </div>
 
   </div>

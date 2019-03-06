c19337a914ae0fbac15d1e5a9af7083251a311b5
OlliverLight <48206915+OlliverLight@users.noreply.github.com>
2019-03-06 09:49:35 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -2,7 +2,18 @@
   require "header.php";
 ?>
         <main>
-
+          <div class="wrapper-main">
+            <section class="section-default">
+              <h1>Signup</h1>
+              <form action="Includes/signup.inc.php" method="POST">
+                <input type="text" name="uid" placeholder="Username">
+                <input type="text" name="mail" placeholder="E-mail">
+                <input type="password" name="pwd" placeholder="Password">
+                <input type="password" name="pwd-repeat" placeholder="Repeat password">
+                <button type="submit" name="signup-submit">Signup</button>
+              </form>
+            </section>
+          </div>
         </main>
 <?php
   require "footer.php";

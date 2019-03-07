201f159f67d1b3f2cc414ccdc198cdd3895b8f9d
gabbe-k <44163424+gabbe-k@users.noreply.github.com>
2019-03-07 21:54:31 +0100

    checkmark styling completed

--- view/signup.php
+++ view/signup.php
@@ -20,7 +20,14 @@
             <input type="text" name="mail" placeholder="E-mail">
             <input type="password" name="pwd" placeholder="Password">
             <input type="password" name="pwd-repeat" placeholder="Repeat password">
-            <input type="checkbox" name="isTeachr" value="1">
+            <div class="checkbox-wrap">
+              <div class="checkbox-text">
+                <p>Would you like to sign up as a teacher?</p>
+              </div>
+              <div class="checkbox">
+                <input type="checkbox" name="isTeachr" value="1">
+              </div>
+            </div>
             <input type="submit" name="signup-submit" value="Submit">
           </form>
         </div>
